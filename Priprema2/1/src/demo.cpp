802b7c60b68e4f12b01f0ecfe7c53264534ff513
TazGee <nikolatasic2004@gmail.com>
2025-10-03 21:08:52 +0200

    Initial commit

---
+++ Priprema2/1/src/demo.cpp
@@ -0,0 +1,19 @@
+#include<arduinoPlatform.h>
+#include<tasks.h>
+#include<interrupts.h>
+#include<stdio.h>
+#include<serial.h>
+#include <pwm.h>
+#include<data.h>
+
+extern serial Serial;
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
